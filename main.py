f66673a1a8966d31520a9a1e20d55368315034bd
evokerking <cjpontius10@gmail.com>
2026-03-21 19:01:38 -0400

    init

---
+++ main.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+intents = discord.Intents.default()
+
+intents.message_content = True
+
+bot = commands.Bot(";", intents=intents)
+copyright = "©Lunx Studios"
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+
+    await bot.change_presence(
+    status=discord.Status.online,
+        activity=discord.Activity(
+            type=discord.ActivityType.playing,
+            name="Jokes on people"
+        )
+    )
+
+
+@bot.command()
+async def ban(ctx, member: discord.Member, *, reason: str = "No reason provided"):
+    embed = discord.Embed(
+        title="🔨 User Banned!",
+        description=f"**{member.display_name}** has been banned from the server!",
+        color=discord.Color.red()
+    )
+    embed.add_field(name="Banned™ User", value=member.mention, inline=True)
+    embed.add_field(name="Banned™ By", value=ctx.author.mention, inline=True)
+    embed.add_field(name="Reason", value=reason, inline=False)
+    embed.set_thumbnail(url=member.display_avatar.url)
+    embed.set_footer(text=copyright)
+
+    await ctx.reply(embed=embed)
+
+bot.run(os.getenv("TOKEN"))
